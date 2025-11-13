e730e29748bb95eda89fd3e1275b6ef862bc25c7
amina339 <unicorn23.ru@gmail.com>
2025-11-13 21:52:13 +0300

    Созданы классы курсов

---
+++ StudyCourseSystem/Courses_Classes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCourseSystem
+{
+    internal class Course_Abstract_Class
+    {
+    }
+}
